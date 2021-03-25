f2bc4229aeea24f2d3248e5a957c819be22c4dd7
Juan Cruz <juandonato@gmail.com>
2021-03-25 14:41:05 -0300

    Fetched pcis data when Provider finishes rendering.

--- src/appContext.js
+++ src/appContext.js
@@ -1,12 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 const AppContext = React.createContext()
 
+const dataURL = 'https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json'
+
 function AppContextProvider(props) {
-    const [ context, setContext ] = useState("")
+
+    const [ context, setContext ] = useState({
+      photos: []
+    })
 
     function changeContext() {
-      setContext("")
+      setContext({
+        photos: []
+      })
     }
+
+    useEffect(() => {
+      fetch(dataURL)
+        .then(response => response.json())
+        .then(data => {
+          setContext({
+            photos: data
+          })
+        })
+    }, [])
 
     return (
       <AppContext.Provider value={{context, changeContext}}>
