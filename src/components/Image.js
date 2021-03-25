4be2965d38f129967bd56aaa2c26112af907e6a2
Juan Cruz <juandonato@gmail.com>
2021-03-25 20:38:13 -0300

    Added heart and plus icon to Image, on hover

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState }from 'react'
 import '../css/Image.css'
 
 function Image({className, img}) {
+
+  const [ hovered, setHovered ] = useState(false)
+
+  const enableHovered = () => setHovered(true)
+
+  const disableHovered = () => setHovered(false)
+
   return (
     <div className={`${className} image-container`}>
-      <img src={img.url} alt='' className="image-grid"/>
+      <img src={img.url} alt='' className="image-grid" onMouseEnter={enableHovered} onMouseLeave={disableHovered}/>
+      {hovered && <i className="ri-heart-line favorite" onMouseEnter={enableHovered} onMouseLeave={disableHovered}></i>}
+      {hovered && <i className="ri-add-circle-line cart" onMouseEnter={enableHovered} onMouseLeave={disableHovered}></i>}
     </div>
   )
 }
