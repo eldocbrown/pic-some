a7920a774fa4dfd48d87808b512bcb56a1076088
Juan Cruz <juandonato@gmail.com>
2021-03-25 16:36:05 -0300

    Rendering images in home page.

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,7 +4,7 @@ import '../css/Image.css'
 function Image({className, img}) {
   return (
     <div className={`${className} image-container`}>
-      <img src={img.url} alt='picture' className="image-grid"/>
+      <img src={img.url} alt='' className="image-grid"/>
     </div>
   )
 }
