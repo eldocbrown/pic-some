b1d5199a3c04c8484c8b4f33f94d30406b7f87b8
Juan Cruz <juandonato@gmail.com>
2021-03-26 14:47:48 -0300

    Styled fovering no order button

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,18 +1,34 @@
-import React, { useContext } from "react"
+import React, { useState, useContext } from "react"
 
 import CartItem from '../components/CartItem'
 import { AppContext } from '../appContext'
+import '../css/Cart.css'
 
 function Cart() {
 
   const FIXED_PRICE = 5.99
 
+  const TIMEOUT = 3000
+
   const { cartItems, removeItemFromCart } = useContext(AppContext)
+
+  const [ ordering, setOrdering ] = useState(false)
 
   const cartItemElements = cartItems.map((item) => <CartItem key={item.id} item={item} removeItemFromCart={removeItemFromCart}/>)
 
   const orderPrice = cartItems.length * FIXED_PRICE
+
   const orderPriceDisplay = orderPrice.toLocaleString("en-US", {style: "currency", currency: "USD"})
+
+  const orderButtonDisplay = () => ordering ? 'Ordering...' : 'Place Order'
+
+  const handlePlaceOrderClick = () => { // Simulate a place order
+    setOrdering(true)
+    setTimeout(() => {
+      cartItems.map((item) => removeItemFromCart(item))
+      setOrdering(false)
+    }, TIMEOUT)
+  }
 
   return (
     <main className="cart-page">
@@ -20,7 +36,7 @@ function Cart() {
       {cartItemElements}
       <p className="total-cost">Total: {orderPriceDisplay}</p>
       <div className="order-button">
-        <button>Place Order</button>
+        <button onClick={handlePlaceOrderClick} disabled={ordering}>{orderButtonDisplay()}</button>
       </div>
     </main>
   )
